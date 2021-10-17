dc5adf0e0fee05cf49037d3057152c75f964ab9f
sandeepsannap <sandeep50sannap@gmail.com>
2021-10-17 11:05:34 +0530

    user model fix

--- models/user.js
+++ models/user.js
@@ -5,7 +5,13 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
     password: { type: String, required: true },
     role: { type: String, default: "customer" },
   },
